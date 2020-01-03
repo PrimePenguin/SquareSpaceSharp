d833085f4adf3c0695080a61612630171a835bad
khungersumit <36700687+Khungersumit@users.noreply.github.com>
2020-01-03 18:36:10 +0530

    pp-update-fulfillment-entity

--- SquareSpaceSharp/Entities/Fulfillment.cs
+++ SquareSpaceSharp/Entities/Fulfillment.cs
@@ -5,19 +5,31 @@ namespace SquareSpaceSharp.Entities
     public class Fulfillment
     {
         /// <summary>
-        /// The name of the shipping company handling the shipment.
+        /// The ISO 8601 date and time representing the moment the shipment
+        /// </summary>
+        [JsonProperty("shipDate")]
+        public string ShipDate { get; set; }
+
+        /// <summary>
+        /// A string representing the parcel service transporting the shipment.
         /// </summary>
         [JsonProperty("carrierName")]
         public string CarrierName { get; set; }
 
         /// <summary>
-        /// The shipping company's parcel tracking number.
+        /// A string representing the level of service, as offered by the carrier, used for this shipment.
+        /// </summary>
+        [JsonProperty("service")]
+        public string Service { get; set; }
+
+        /// <summary>
+        /// A string representing the carrier-generated tracking number.
         /// </summary>
         [JsonProperty("trackingNumber")]
         public string TrackingNumber { get; set; }
 
         /// <summary>
-        /// The tracking URL for the shipment.
+        /// Optional A tracking URL, ideally supplied by the carrier. If this value is provided, it must represent a valid URL.
         /// </summary>
         [JsonProperty("trackingUrl")]
         public string TrackingUrl { get; set; }
